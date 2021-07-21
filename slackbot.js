7d8827ca38aa6b3471ad08317e423e1347591c7a
김창회 <dudnqnfqlc@naver.com>
2021-07-21 19:58:57 +0900

    한국시간 받도록 설정

--- slackbot.js
+++ slackbot.js
@@ -3,11 +3,18 @@ const { RTMClient } = require("@slack/client")
 const token = process.env.SLACK_TOKEN
 const rtm = new RTMClient(token)
 
+const getKtime = () => {
+    const timestamp =
+        Date.now() + new Date().getTimezoneOffset() * 60000 + 9 * 3600 * 1000
+    return new Date(timestamp)
+}
+
 async function sendByWorking() {
     rtm.on("message", async (event) => {
         let text = event.text ? event.text : event.message.text
+
         const channel = event.channel
-        const date = new Date()
+        const date = getKtime()
         const hours = date.getHours()
         const minutes = date.getMinutes()
 
