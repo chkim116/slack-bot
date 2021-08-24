3cdbf8040d2c02bc0c844828fa18fbde99958a6d
김창회 <dudnqnfqlc@naver.com>
2021-08-25 05:32:57 +0900

    yarn.lock 삭제

--- crawling/bestKeywordCrawling.js
+++ crawling/bestKeywordCrawling.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer")
 const fs = require("fs")
 const { getKeywordInfo } = require("./getKeywordInfo")
 const { launchNaver } = require("./launchNaver")
+
 const pageWithoutAssets = async (page) => {
     await page.setRequestInterception(true)
 
