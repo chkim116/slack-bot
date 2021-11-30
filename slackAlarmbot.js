e10a51f7e71f06bbddb6c45346a7dc35c66af0ef
김창회 <dudnqnfqlc@naver.com>
2021-11-30 18:20:23 +0900

    슬랙 메세지 보내는 기능 추가

---
+++ slackAlarmbot.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const { WebClient } = require("@slack/client");
+const { WORK_TOKEN } = process.env;
+
+const web = new WebClient(WORK_TOKEN);
+
+async function postSlackMessage(channelName, text) {
+    try {
+        let channelId;
+
+        const result = await web.conversations.list({
+            token: WORK_TOKEN,
+        });
+
+        for (const channel of result.channels) {
+            if (channel.name === channelName) {
+                conversationId = channel.id;
+                channelId = conversationId;
+                break;
+            }
+        }
+
+        if (channelId) {
+            const res = await web.chat.postMessage({
+                token: WORK_TOKEN,
+                channel: channelId,
+                text,
+            });
+            console.log(res);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    postSlackMessage,
+};
