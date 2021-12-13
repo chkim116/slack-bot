b39d7c168db25b07625c25aa5a05d413ff8700c4
김창회 <dudnqnfqlc@naver.com>
2021-12-13 11:03:31 +0900

    12.13 크롤

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const http = require("http");
 const nodeSchedule = require("node-schedule");
 const { workEnd, workStart } = require("./slackSlashbot");
+const { postSlackMessage } = require("./slackAlarmbot");
 const { scrapLaunch } = require("./scrap");
 const cors = require("cors");
 
@@ -27,6 +28,26 @@ app.post("/slack/work/end", workEnd);
 app.get("/kw", (req, res) => {
     const data = require("./lib/kw.json");
     res.status(200).send(data);
+});
+app.get("/scrap", async (req, res) => {
+    try {
+        await scrapLaunch();
+        await postSlackMessage(
+            "개발",
+            `${new Date().toLocaleDateString()}자 황금 키워드 추출 완료 ${(
+                (endTime - startTime) /
+                1000
+            ).toFixed()}초 소요. 황금키워드는 총 ${
+                res.length
+            }개, 전날 없던 키워드는 ${
+                res.filter((lst) => lst.isNew).length
+            }개 입니다.`
+        );
+        res.status(200);
+    } catch (err) {
+        console.error(err);
+        await postSlackMessage("개발", "실패");
+    }
 });
 app.get("/", (req, res) => res.send("이곳은 얼리21의 슬랙 앱입니다."));
 
